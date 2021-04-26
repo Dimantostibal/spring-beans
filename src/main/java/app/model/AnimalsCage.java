81dd8760afa58c6895090115a1f2fa1b47036a3f
Dimantostibal <Dimantostibal@mail.ru>
2021-04-27 00:35:32 +0300

    complete

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,28 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
 
     @Autowired
-    private Animal animal;
+    @Qualifier("myTimer")
+    private MyTimer myTimer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(myTimer.getTime());
         System.out.println("________________________");
+    }
+
+    public MyTimer getTimer() {
+        return myTimer;
     }
 }
